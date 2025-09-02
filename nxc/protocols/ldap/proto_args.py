9cd1e27d6bf7270112f3261505d19cd109681c66
f3rn0s <ffc@null.net>
2025-09-02 15:49:15 +1000

    Spelling mistakes

--- nxc/protocols/ldap/proto_args.py
+++ nxc/protocols/ldap/proto_args.py
@@ -42,6 +42,6 @@ def proto_args(parser, parents):
     bgroup.add_argument("-c", "--collection", default="Default", help="Which information to collect. Supported: Group, LocalAdmin, Session, Trusts, Default, DCOnly, DCOM, RDP, PSRemote, LoggedOn, Container, ObjectProps, ACL, All. You can specify more than one by separating them with a comma")
     bgroup.add_argument("--dc", help="Override the DC")
     bgroup.add_argument("--gc", help="Override the GC")
-    bgroup.add_argument("--channel-binding", action="store_true", help="Wheter or not to use Channel Binding")
+    bgroup.add_argument("--channel-binding", action="store_true", help="Enable Channel Binding for Bloodhound")
 
     return parser
